e3ee008d9f9a056cac289c083c692a9d0af3ee52
Pat Nakajima <patnakajima@gmail.com>
2008-03-07 02:22:52 -0500

    added basic plans file to doc

---
+++ doc/blog_plans.rb
@@ -0,0 +1,28 @@
+class Post < ActiveRecord::Base
+  
+end
+
+class Article < Post
+  
+end
+
+class Tweet < Post
+  
+end
+
+class Snippet < Post
+  
+end
+
+class Picture < Post
+  
+end
+
+class Quote < Post
+  
+end
+
+class Link < Post
+  
+end
+
