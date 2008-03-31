330b96559a752ad4637c66cf7c5816b7e8cc3e51
Pat Nakajima <patnakajima@gmail.com>
2008-03-31 18:25:48 -0400

    added import_twitter_replies option to SITE_SETTINGS

--- app/models/posts/tweet.rb
+++ app/models/posts/tweet.rb
@@ -1,5 +1,11 @@
 class Tweet < Post
   
   validates_presence_of :content
+  validate :check_if_reply
+  
+  def check_if_reply
+    return true if SITE_SETTINGS['import_twitter_replies']
+    self.errors.add_to_base("This was a direct reply.") if content.match(/\A@\w+:/)
+  end
 
 end
