3d0ee5a7731ea0e4ba9cc8933e7fc38d82225c57
Pat Nakajima <patnakajima@gmail.com>
2008-04-23 19:44:55 -0400

    oops, trying to set created_at for tweets again

--- app/models/feeds/twitter.rb
+++ app/models/feeds/twitter.rb
@@ -5,6 +5,7 @@ class Twitter < Feed
   def refresh!
     entries.each do |entry|
       tweet = tweets.build :permalink => entry.urls.first, :content => entry.content.gsub(/\A\w*:\s/, '')
+      tweet.created_at = entry.try(:date_published)
       tweet.save
     end
   end
