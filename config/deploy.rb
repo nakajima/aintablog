b2b4688a1f483f175548489aea071b1e1e012c68
Pat Nakajima <patnakajima@gmail.com>
2008-05-13 23:33:32 -0400

    fixed current_path typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 namespace :cache do
   desc "Remove cache directory, essentially expiring the whole thing"
   task :expire do
-    run "rm -rf #{current_directory}/public/cache"
+    run "rm -rf #{current_path}/public/cache"
   end
 end
