759b989d18d285746c92c2e68055c005e818b410
Pat Nakajima <patnakajima@gmail.com>
2008-07-11 13:28:07 -0400

    cleaned up page caching assertions

--- test/integration/post_caching_integration_test.rb
+++ test/integration/post_caching_integration_test.rb
@@ -10,47 +10,56 @@ class PostCachingIntegrationTest < ActionController::IntegrationTest
   end
   
   def test_should_page_cache_posts_index
-    assert_paths_cached('/index') do
+    assert_paths_cached('/index.html') do
       get '/'
     end
   end
   
   def test_should_page_cache_articles_index
-    assert_paths_cached('/articles')
+    assert_paths_cached('/articles.html') do
+      get '/articles'
+    end
   end
   
   def test_should_page_cache_snippets_index
-    assert_paths_cached('/snippets')
+    assert_paths_cached('/snippets.html') do
+      get '/snippets'
+    end
   end
   
   # Show Caches
   
   def test_should_page_cache_article_show_page
     article = posts(:article)
-    assert_paths_cached("/articles/#{article.permalink}")
+    assert_paths_cached("/articles/#{article.permalink}.html") do
+      get "/articles/#{article.permalink}"
+    end
   end
   
   def test_should_page_cache_snippet_show_page
     snippet = posts(:snippet)
-    assert_paths_cached("/snippets/#{snippet.permalink}")
+    assert_paths_cached("/snippets/#{snippet.permalink}.html") do
+      get "/snippets/#{snippet.permalink}"
+    end
   end
   
   def test_should_page_cache_post_page_links
-    assert_paths_cached("/posts/page/1")
+    assert_paths_cached("/posts/page/1.html") do
+      get '/posts/page/1'
+    end
   end
   
   def test_should_page_cache_articles_page_link
-    assert_paths_cached("/articles/page/1")
+    assert_paths_cached("/articles/page/1.html") do
+      get '/articles/page/1'
+    end
   end
   
   # Expiration Tests ---------------------------------------------------
   
   def test_should_be_able_to_expire_by_path
-    get '/'
-    get '/posts/page/1'
-    get '/articles'
-    get '/articles/page/1'
-    assert_cache_files_deleted('/index', '/posts/', '/articles/', '/articles') do
+    get_paths '/', '/posts/page/1', '/articles', '/articles/page/1'
+    assert_cache_expired('/index.html', '/posts/', '/articles/', '/articles') do
       @controller.expire_path('/index.html')
       @controller.expire_path('/posts')
       @controller.expire_path('/articles.html')
@@ -59,10 +68,7 @@ class PostCachingIntegrationTest < ActionController::IntegrationTest
   end
   
   def test_should_expire_index
-    get '/'
-    get '/posts/page/1'
-    get '/articles'
-    get '/articles/page/1'
+    get_paths '/', '/posts/page/1', '/articles', '/articles/page/1'
     assert_cache_expired('/index.html', '/posts', '/articles', '/articles.html') do
       @controller.instance_variable_set("@post", posts(:article))
       @controller.send :expire_index!
@@ -72,52 +78,61 @@ class PostCachingIntegrationTest < ActionController::IntegrationTest
   # Expiring main index
   
   def test_should_expire_home_page_on_post_create
-    login_as :quentin
-    post '/posts', :post => { :type => 'Article', :header => 'Something', :content => 'Something else' }
+    get_paths '/', '/posts/page/1', '/articles', '/articles/page/1'
+    assert_cache_expired('index.html', 'posts/', 'articles/', 'articles.html') do
+      login_as :quentin
+      post '/posts', :post => { :type => 'Article', :header => 'Something', :content => 'Something else' }
+    end
   end
   
   def test_should_expire_home_page_on_post_update
     article = posts(:article)
-    login_as :quentin
-    put "/articles/#{article.permalink}", :article => { :content => 'well this is different' }
-    assert_redirected_to post_path(article)
+    get_paths '/', '/posts/page/1', '/articles', '/articles/page/1'
+    assert_cache_expired('index.html', 'posts/', 'articles/', 'articles.html') do
+      login_as :quentin
+      put "/articles/#{article.permalink}", :article => { :content => 'well this is different' }
+      assert_redirected_to post_path(article)
+    end
   end
   
   def test_should_expire_home_page_on_post_destroy
     article = posts(:article)
-    login_as :quentin
-    delete "/articles/#{article.permalink}"
+    get_paths '/', '/posts/page/1', '/articles', '/articles/page/1'
+    assert_cache_expired('index.html', 'posts/', 'articles/', 'articles.html') do
+      login_as :quentin
+      delete "/articles/#{article.permalink}"
+    end
   end
   
   def teardown
-    wipe_cache!
+    # wipe_cache!
   end
 
   private
+  
+  def get_paths(*urls)
+    urls.each { |url| get url }
+  end
   
   def login_as(name)
     post session_path, :email => users(name).email, :password => 'test'
   end
   
   def assert_paths_cached(*urls)
-    urls.each { |url| assert ! cache_exists_for?(url), "cache should not exist yet. remove public#{url}.html" }
-    block_given? ? yield : urls.each { |url| get url }
+    raise "You must pass a block" unless block_given?
+    urls.each { |url| assert ! cache_exists_for?(url), "cache should not exist yet. remove public#{url}" }
+    yield
     urls.each { |url| assert cache_exists_for?(url), "cache not generated for #{url}" }
   end
   
-  def assert_cache_files_deleted(*urls)
-    urls.each { |url| assert cache_exists_for?(url), "cache should already exist. missing: public#{url}.html" }
+  def assert_cache_expired(*urls)
+    urls.each { |url| assert cache_exists_for?(url), "cache should already exist. missing: public#{url}" }
     block_given? ? yield : urls.each { |url| get url }
     urls.each { |url| assert ! cache_exists_for?(url), "cache not expired for #{url}" }
   end
   
-  def assert_cache_expired(*urls)
-    urls.each { |url| FileUtils.expects(:rm_rf).with(RAILS_ROOT + '/public' + url) }
-    yield
-  end
-  
   def cache_exists_for?(file)
-    File.exists?(RAILS_ROOT + '/public' + (file.match(/\/$/) ? (file[0..(file.length - 2)]) : "#{file}.html"))
+    File.exists?(RAILS_ROOT + '/public/' + file)
   end
   
   def wipe_cache!
