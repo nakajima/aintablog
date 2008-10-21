72338a29455389ed727ca36cc84907ac5ada246e
nakajima <patnakajima@gmail.com>
2008-10-20 23:16:31 -0700

    using dsl-ified syntax

--- app/models/feeds/github.rb
+++ app/models/feeds/github.rb
@@ -1,17 +1,13 @@
 class Github < Feed
   
-  entries_become :gists
-  
-  def refresh!
-    entries.each do |entry|
-      gist = gists.build :content => entry.content, :header => entry.title
-      gist.permalink  = entry.urls.first
-      gist.created_at = entry.try(:date_published)
-      gist.updated_at = entry.try(:last_updated)
-      gist.save
-    end
-    update_attribute :updated_at, Time.now
+  entries_become :gists do |gist, entry|
+    gist.content = entry.content
+    gist.header = entry.title
+    gist.permalink  = entry.urls.first
+    gist.created_at = entry.try(:date_published)
+    gist.updated_at = entry.try(:last_updated)
+    gist.save
   end
-  
 
 end
+
