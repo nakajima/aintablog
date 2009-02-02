c55cc35a5677159b27bca72323d6d69bc3d53e6c
Pat Nakajima <patnakajima@gmail.com>
2009-02-01 23:47:13 -0500

    specify RAILS_GEM_VERSION explicitly, updated dependencies in README

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -66,7 +66,7 @@ class SessionsControllerTest < ActionController::TestCase
   def test_should_delete_token_on_logout
     login_as :quentin
     get :destroy
-    assert_equal @response.cookies["auth_token"], []
+    assert ! @response.cookies["auth_token"]
   end
 
   def test_should_login_with_cookie
